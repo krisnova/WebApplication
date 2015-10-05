adf371715199b71d9a2dd2011f96a8afc16d0a35
Hannah Newsome <h.e.newsome@gmail.com>
2015-10-05 00:11:28 -0700

    adding a few basic routes and templates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                hello ;)
             </div>
         </div>
     </body>
