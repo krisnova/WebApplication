2a4559ca8e28db34a2a8bba3eb6fc361b9a03b36
Sabio <sabiodesigns7@gmail.com>
2016-01-12 19:05:57 +0000

    Testing Area 2.5: Collective SOLVED

--- config/app.php
+++ config/app.php
@@ -144,6 +144,9 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        //Laravel Collective Additions
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -193,6 +196,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        //Laravel Collective Additions
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
