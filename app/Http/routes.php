c512053a4ecd03e18e4a06b677d5b1eb41b061d2
sabio-designs <sabio@sabiodesigns.com>
2016-01-12 01:27:25 -0500

    Adding in the Testing/Upload Area

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,6 +41,10 @@ Route::get('profile', [
 Route::get('upload', 'UploadController@index');
 Route::post('upload/add', 'UploadController@uploadFiles');
 
+Route::get('testing-site', function(){
+    return view('testing-site');
+});
+
 Route::get('matchmap', function(){
     return view('matchmap');
 });
