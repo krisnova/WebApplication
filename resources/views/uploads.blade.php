aab37665bad7cc4214bf2a7db399ef28da7f3ecf
sabio-designs <sabio@sabiodesigns.com>
2016-01-12 12:36:48 -0500

    Testing Area #1.8
    Adding some needed verification for files.

--- resources/views/uploads.blade.php
+++ resources/views/uploads.blade.php
@@ -7,6 +7,7 @@
      <hr />
 
     <form action="add" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="file" name="file">
         <input type="submit">
     </form>
