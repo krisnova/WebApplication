2e99d3c9a7192786bf22f6c5910f749d99a9c0cf
sabio-designs <sabio@sabiodesigns.com>
2016-01-12 13:31:03 -0500

    Testing Area #2.2: Collective
    Troubleshooting the same error

--- config/app.php
+++ config/app.php
@@ -196,7 +196,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        /*Laravel Collective Additions*/
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 
