7c2989cfe352ca8c8734b2031f6e4b0e1a005e04
sabio-designs <sabio@sabiodesigns.com>
2016-01-12 13:25:24 -0500

    Testing Area #2.1 Collective
    Troubleshooting an error “PHP Fatal error:  Class
    'Collective\Html\HtmlServiceProvider'
       not found in
    /home/sabio/WebApplication-restore/vendor/laravel/framework/s
      rc/Illuminate/Foundation/ProviderRepository.php on line 146”

--- config/app.php
+++ config/app.php
@@ -145,7 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        //Laravel Collective Additions
+        /*Laravel Collective Additions*/
         Collective\Html\HtmlServiceProvider::class,
 
     ],
@@ -196,7 +196,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        //Laravel Collective Additions
+        /*Laravel Collective Additions*/
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 
