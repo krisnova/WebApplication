62255bf8bae087f6962144c3863d3bce9ae4d942
Sabio <sabiodesigns7@gmail.com>
2016-01-13 17:27:57 +0000

    Sabio Splash Page0.1

---
+++ resources/views/splash.blade.php
@@ -0,0 +1,344 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Soundeavor</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="{{ URL::asset('sabiosplash/assets/js/ie/html5shiv.js') }}"></script><![endif]-->
+		<link rel="stylesheet" href="{{ URL::asset('sabiosplash/assets/css/main.css') }}" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="{{ URL::asset('sabiosplash/assets/css/ie8.css') }}" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="{{ URL::asset('sabiosplash/assets/css/ie9.css') }}" /><![endif]-->
+        <!--Favicon -->
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        
+        <!--mailchimp signup styles-->
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            #mc_embed_signup{background:rgba(255, 0, 0, 0.0); clear:left; font:14px Helvetica,Arial,sans-serif; }
+            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        </style>
+	</head>
+	<body class="landing">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="#banner">SOUNDEAVOR</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="#banner">Home</a></li>
+											<li><a href="#one">The Science</a></li>
+											<li><a href="#two">Why?</a></li>
+											<li><a href="#cta">Get Involved</a></li>
+											<li><a href="#">Register</a></li>
+											<li><a href="#">Log In</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<!--<h2>SOUNDEAVOR</h2>-->
+							<img id="banner-logo" src="{{ URL::asset('sabiosplash/images/soundeavor-logo.png') }}" />
+							<p>Unlocking musical potential <br />through analytics, collaboration and competition.
+							</p>
+							<ul class="actions">
+								<li><a href="#one" class="button special">What's This Wizardry?</a></li>
+							</ul>
+						</div>
+						<a href="#one" class="more scrolly">Learn More</a>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper special soundeavor-red-background">
+						<div class="inner ">
+							<header class="major">
+								<ul class="icons">
+									<li><span class="icon fa-microphone-slash major style5"><span class="label">The Music Industry Is Broken</span></span></li>
+								</ul>
+								<h2>The music industry is broken</h2>
+								<p>It's no longer the good old days. There's no guy in the corner of the bar waiting<br /> to sign the next big thing.
+								It's a struggle, a daily grind. You have to be a musician,<br /> a salesman, and more, just to be heard.</p>
+								<ul class="icons">
+									<li><span class="icon fa-heart-o major style1"><span class="label">The Music Industry Is Broken</span></span></li>
+								</ul>
+								<h2>We hear you</h2>
+								<p>And we want you to be heard. From the beginning bassist who just learned <br />
+									that Every Good Boy Does Fine (we're not condoning that motto), to the world touring <br />
+									producer, Soundeavor knows you can do more. And we're here to help. After all, we're a <br />
+									bunch of crazy musicians just like you.</p>
+							</header>
+
+					<!--		<ul class="icons major">
+								<li><span class="icon fa-microphone-slash major style1"><span class="label">Lorem</span></span></li>
+								<li><span class="icon fa-hand-spock-o major style1"><span class="label">Lorem</span></span></li>
+								<li><span class="icon fa-heart-o major style1"><span class="label">We Hear You</span></span></li>
+								<li><span class="icon fa-code major style3"><span class="label">Dolor</span></span></li>
+							</ul>-->
+						</div>
+						<a href="#two" class="more scrolly">Sounds Good, <br /> but how?</a>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper alt soundeavor-gray-background">
+						<header class="major special align-center">
+							<h2>We’ve Got The Formula
+							</h2>
+							<p>And We’re Popping the Bottle for All to Enjoy</p>
+							<li style="list-style:none"><span class="icon fa-flask fa-5x major style1"><span class="label">Popping the Bottle</span></span></li>
+
+					</header>
+						<section class="spotlight">
+							<div class="image"><img src="{{ URL::asset('sabiosplash/images/pic01.jpg') }}" alt="" /></div><div class="content">
+
+								<h2>It's Not...Yes It Is
+								</h2>
+								<p>Here at Soundeavor, our rocket scientists (yes, really!) have developed a formula
+									unearthing the fundamental qualities of music and its sound. But that’s not all.
+									We want to share it with YOU. We envision a world where the right music meets
+									the right ear, every time, with less effort on all sides.
+								</p>
+									<p>That’s right, no more abondoned country hoe downs for your touring punk band.
+										And yes, no more Justin Bieber cover bands opening for your hip hop project
+										(I know, they were supposed to bring a crowd).</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="{{ URL::asset('sabiosplash/images/pic02.jpg') }}" alt="" /></div><div class="content">
+								<h2>Together, we are the future</h2>
+								<p>It’s happened to us all, and we know it’s no one’s fault. Instead of pointing fingers, getting bummed, breaking up the band,
+									let’s work together to fix this. Our algorithms help create matching elements between musicians, venues, and labels…in any
+									 which way you can imagine.</p>
+									 <p>Always wanted to have a band of twenty-two sousaphonists? A venue in search of the perfect balance between
+									 jam and electronic? An up and coming label looking to carve out a unique sculpture in the giving tree of music? We got you.
+									 And you got us. We can’t do this without you, because…well…a network with no net…doesn’t work (bad pun intended).</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="{{ URL::asset('sabiosplash/images/pic03.jpg') }}" alt="" /></div><div class="content">
+								<h2>Rocket Science. Dead Simple.</h2>
+								<p>We think music matching should be easy. Just plug in your instrument and play. Don’t have it on you? Don’t use the analogs?
+									Upload a file. We’ll do the math, and with some of your taste and inspiration guidance, introduce the musical seas to your
+									very own sound. </p>
+								<p>From the other end of the wormhole, in search of a sound in the sea, our formula dissolves the guesswork and provides
+									real-time detailed insights on how different qualities of the sound match your desired criteria. </p>
+							</div>
+						</section>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2>And there’s more</h2>
+								<p>We believe there is a ton of potential for what we’ve created,
+									from all regions of all kinds of music-related atmospheres,
+									from Juilliard to Sector 9. But that doesn’t mean we’ve thought of everything. Far from it.
+
+									<p>We need you. Have thoughts?
+									Want to make sure we don’t forget a killer feature? Someone you know that we <i>have</i> to meet? We have a distributed team
+									of specialists across North America transforming code into future, and they know better than anyone that there’s always
+									room for more practice to make perfect.</p>
+								 <img id="map" src="{{ URL::asset('sabiosplash/images/soundeavor_map-trans.png') }}" />
+
+							</header>
+				<!--			<i class="fa fa-map-marker fa-5x"></i>
+							<h2>Click to Get in Touch with the Team</h2> -->
+							<div class="col-md-4 col-xs-1">
+							<a href="mailto:sanfrancisco@soundeavor.com"><button>San Francisco</button></a>
+							<a href="mailto:denver@soundeavor.com"><button>Denver</button></a>
+							<a href="mailto:delta@soundeavor.com"><button>Mississippi Delta</button></a>
+							<a href="mailto:baltimoreo@soundeavor.com"><button>Baltimore</button></a>
+							</div>
+
+						</div>
+					</section>
+<!--
+							<ul class="features">
+								<li class="icon fa-paper-plane-o">
+									<h3>Arcu accumsan</h3>
+									<p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+								</li>
+								<li class="icon fa-laptop">
+									<h3>Ac Augue Eget</h3>
+									<p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+								</li>
+								<li class="icon fa-code">
+									<h3>Mus Scelerisque</h3>
+									<p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+								</li>
+								<li class="icon fa-headphones">
+									<h3>Mauris Imperdiet</h3>
+									<p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+								</li>
+								<li class="icon fa-heart-o">
+									<h3>Aenean Primis</h3>
+									<p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+								</li>
+								<li class="icon fa-flag-o">
+									<h3>Tortor Ut</h3>
+									<p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+								</li>
+							</ul>
+-->
+
+
+					<!-- Four -->
+						<section id="four" class="wrapper special soundeavor-red-background">
+							<div class="inner ">
+								<header class="major">
+									<ul class="icons">
+										<li><span class="icon fa-hand-spock-o major style5"><span class="label">Work better with your hands?</span></span></li>
+									</ul>
+									<h2>Work better with your hands?</h2>
+									<p>[Insert guitarist joke.] As we begin to roll out our alpha and beta versions of the Soundeavor platform,
+										we’ll need testers. No matter where you fall in the music spectrum, we can use your eyes and ears to improve
+										and evolve the application into something that will change music forever.</p>
+										<!-- Begin MailChimp Signup Form
+<div id="mc_embed_signup">
+<form action="//soundeavor.us12.list-manage.com/subscribe/post?u=9fd823286f4df1d7b20a93bc2&amp;id=4a1ae56f21" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group">
+	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-MMERGE3">Relationship to Music  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="MMERGE3" class="required" id="mce-MMERGE3">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9fd823286f4df1d7b20a93bc2_4a1ae56f21" tabindex="-1" value=""></div>
+		<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+
+	  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+						</section>
+
+				<!-- CTA -->
+					<section id="cta" class="wrapper style4">
+						<div class="inner">
+							<header>
+								<h2>Are You With Us?</h2>
+								<p>Be on the cutting edge of the music industry...the new one we're building together.</p>
+								<p>Your name and email gets you there, and puts you first in line for consideration in our testing programs.</p>
+								<!-- Begin MailChimp Signup Form -->
+								
+								<div id="mc_embed_signup">
+								<form action="//soundeavor.us12.list-manage.com/subscribe/post?u=9fd823286f4df1d7b20a93bc2&amp;id=223869dd75" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								    <div id="mc_embed_signup_scroll">
+								<!--	<h2>Subscribe to our mailing list</h2>-->
+								<div class="mc-field-group">
+									<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+								</label>
+									<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+								</div>
+								<div class="mc-field-group">
+									<label for="mce-FNAME">First Name  <span class="asterisk">*</span></label>
+									<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+								</div>
+								<div class="mc-field-group">
+									<label for="mce-LNAME">Last Name <span class="asterisk">*</span></label>
+									<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+								</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+								    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9fd823286f4df1d7b20a93bc2_223869dd75" tabindex="-1" value=""></div>
+										<div class="indicates-required" style="float:right;"><span class="asterisk">*</span> indicates required</div>
+									  <div class="clear"><input type="submit" value="I'm In" name="subscribe" id="mc-embedded-subscribe" class="button special"></div>
+								    </div>
+								</form>
+								</div>
+
+								<!--End mc_embed_signup-->
+							</header>
+							<ul class="actions vertical">
+								<!--<li><i class="fa fa-codiepie fa-spin"></i></li>-->
+								<li><a href="#" class="button fit special">Activate</a></li>
+								<li><a href="#" class="button fit">Learn More</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+					<!--	<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+							<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul> -->
+						<ul class="copyright">
+							<li>&copy; Soundeavor 2015.</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="{{ URL::asset('sabiosplash/assets/js/jquery.min.js') }}"></script>
+			<script src="{{ URL::asset('sabiosplash/assets/js/jquery.scrollex.min.js') }}"></script>
+			<script src="{{ URL::asset('sabiosplash/assets/js/jquery.scrolly.min.js') }}"></script>
+			<script src="{{ URL::asset('sabiosplash/assets/js/skel.min.js') }}"></script>
+			<script src="{{ URL::asset('sabiosplash/assets/js/util.js') }}"></script>
+			<script src="{{ URL::asset('sabiosplash/assets/js/main.js') }}"></script>
+			<!--[if lte IE 8]><script src="{{ URL::asset('sabiosplash/assets/js/ie/respond.min.js') }}"></script><![endif]-->
+
+	</body>
+</html>
